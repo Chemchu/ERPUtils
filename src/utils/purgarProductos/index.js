9b9f9fd445d67f1a3efb3658d0a3249c179346fc
gus <gustavo_9_rj@hotmail.com>
2022-07-12 19:54:56 +0200

    Purgar ya se hace bien

--- src/utils/purgarProductos/index.js
+++ src/utils/purgarProductos/index.js
@@ -5,10 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const transformSalesToJson_1 = require("../transformSalesToJson");
 const fs_1 = __importDefault(require("fs"));
-// No termina nunca de calcular. OPTIMIZAR
 const PurgarProductos = (productosEnDb, ventas) => {
     const productosVendidos = new Map();
-    for (let i = 0; ventas.length; i++) {
+    for (let i = 0; i < ventas.length; i++) {
         if (!ventas[i]) {
             continue;
         }
@@ -19,29 +18,27 @@ const PurgarProductos = (productosEnDb, ventas) => {
             productosVendidos.set(ventas[i].productos[j].ean, ventas[i].productos[j]);
         }
     }
-    const productosEan = Array.from(productosEnDb.keys());
-    for (let index = 0; index < productosEan.length; index++) {
-        if (!productosVendidos.has(productosEan[index])) {
-            productosEnDb.delete(productosEan[index]);
+    const productosFinal = new Map();
+    productosEnDb.forEach((producto, ean) => {
+        if (productosVendidos.has(ean) && !productosFinal.has(ean)) {
+            productosFinal.set(ean, producto);
         }
-    }
-    return Array.from(productosEnDb.values());
+    });
+    return Array.from(productosFinal.values());
 };
 let productosMap = (0, transformSalesToJson_1.ProductosCSVToMap)("productos.csv");
-let ventasSinProdMap = (0, transformSalesToJson_1.VentaXLSXToMap)("ventas.xlsx");
-const ventasConProdMap = (0, transformSalesToJson_1.AddProductosToVentas)(ventasSinProdMap, "productosPorVenta.xlsx", productosMap);
+let ventasMap = (0, transformSalesToJson_1.VentaXLSXToMap)("ventas.xlsx");
+(0, transformSalesToJson_1.AddProductosToVentas)(ventasMap, "productosPorVenta.xlsx", productosMap);
 const prodEanFixed = Array.from(productosMap.values());
 let prodMap = new Map();
 for (let i = 0; i < prodEanFixed.length; i++) {
     prodMap.set(prodEanFixed[i].ean, prodEanFixed[i]);
 }
 let ventas = [];
-ventasConProdMap.forEach((venta, id) => {
+ventasMap.forEach((venta, id) => {
     ventas.push(venta);
 });
-console.log(prodMap.size);
 const productosPurgados = PurgarProductos(prodMap, ventas);
-console.log(productosPurgados.length);
 fs_1.default.writeFile("productosPurgados.json", JSON.stringify(productosPurgados), function (err) {
     if (err) {
         console.log(err);
--- src/utils/purgarProductos/index.ts
+++ src/utils/purgarProductos/index.ts
@@ -2,10 +2,9 @@ import { Producto, ProductoVendido, Venta } from "../../../types";
 import { AddProductosToVentas, ProductosCSVToMap, VentaXLSXToMap } from "../transformSalesToJson";
 import fs from 'fs';
 
-// No termina nunca de calcular. OPTIMIZAR
 const PurgarProductos = (productosEnDb: Map<string, Producto>, ventas: Venta[]): Producto[] => {
     const productosVendidos: Map<string, ProductoVendido> = new Map()
-    for (let i = 0; ventas.length; i++) {
+    for (let i = 0; i < ventas.length; i++) {
         if (!ventas[i]) { continue; }
 
         for (let j = 0; j < ventas[i].productos.length; j++) {
@@ -15,20 +14,19 @@ const PurgarProductos = (productosEnDb: Map<string, Producto>, ventas: Venta[]):
             productosVendidos.set(ventas[i].productos[j].ean, ventas[i].productos[j])
         }
     }
-
-    const productosEan = Array.from(productosEnDb.keys())
-    for (let index = 0; index < productosEan.length; index++) {
-        if (!productosVendidos.has(productosEan[index])) {
-            productosEnDb.delete(productosEan[index])
+    const productosFinal: Map<string, Producto> = new Map()
+    productosEnDb.forEach((producto, ean) => {
+        if (productosVendidos.has(ean) && !productosFinal.has(ean)) {
+            productosFinal.set(ean, producto)
         }
-    }
+    })
 
-    return Array.from(productosEnDb.values())
+    return Array.from(productosFinal.values())
 }
 
 let productosMap = ProductosCSVToMap("productos.csv")
-let ventasSinProdMap = VentaXLSXToMap("ventas.xlsx");
-const ventasConProdMap = AddProductosToVentas(ventasSinProdMap, "productosPorVenta.xlsx", productosMap);
+let ventasMap = VentaXLSXToMap("ventas.xlsx");
+AddProductosToVentas(ventasMap, "productosPorVenta.xlsx", productosMap);
 const prodEanFixed = Array.from(productosMap.values())
 
 let prodMap = new Map();
@@ -37,14 +35,11 @@ for (let i = 0; i < prodEanFixed.length; i++) {
 }
 
 let ventas: Venta[] = []
-ventasConProdMap.forEach((venta, id) => {
+ventasMap.forEach((venta, id) => {
     ventas.push(venta)
 })
 
-console.log(prodMap.size);
 const productosPurgados = PurgarProductos(prodMap, ventas)
-console.log(productosPurgados.length);
-
 fs.writeFile("productosPurgados.json", JSON.stringify(productosPurgados), function (err) {
     if (err) {
         console.log(err);
